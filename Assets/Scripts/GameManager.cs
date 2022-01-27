f554bb7003882f9503f2148e00caf18c8913c358
Junichiro178 <bz.kitm.fj@gmail.com>
2022-01-27 21:36:31 +0900

    ターンカウントの実装
    ターンを数えるメソッドを追加しました

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,72 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // 手札にカードを生成する
 
-    [SerializeField] Transform playerHandTransform;   
+    [SerializeField] Transform playerHandTransform,
+                               enemyHandTransform;
     [SerializeField] CardController cardPrefab;
+
+    // プレイヤーのターンかどうか
+    bool isPlayerTurn;
+
     void Start()
     {
-        CreateCard(playerHandTransform);
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        InitHand();
+        isPlayerTurn = true;
+        TurnCalculation();
+    }
+
+    void InitHand()
+    {
+        // カードをプレイヤーに３枚配る
+        for (int i = 0; i < 3; i++)
+        {
+            CreateCard(playerHandTransform);
+            CreateCard(enemyHandTransform);
+        }
     }
 
      void CreateCard(Transform hand)
     {
         CardController card = Instantiate(cardPrefab, hand, false);
         card.Init(2);
+    }
+
+    // ターンを計算する
+    void TurnCalculation()
+    {
+        if (isPlayerTurn)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+
+    // ターンの切り替え
+    public void ChangeTurn()
+    {
+        isPlayerTurn = !isPlayerTurn;
+        TurnCalculation();
+    }
+
+    void PlayerTurn()
+    {
+        Debug.Log("Playerのターン");
+    }
+
+    void EnemyTurn()
+    {
+        Debug.Log("Enemyのターン");
+
+        ChangeTurn();
     }
 
 }
